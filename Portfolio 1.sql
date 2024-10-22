17bd849d8cfd70d8be683f321f1773007af18dcd
Justin Yu <justinyu6866@gmail.com>
2024-10-22 13:00:23 -0400

    changes to Portfolio 1

--- Portfolio 1.sql
+++ Portfolio 1.sql
@@ -1,4 +1,4 @@
---Task 1 --Create a table on the filegroup
+--Task 1: Create a table on the filegroup
 ALTER DATABASE AdventureWorksLT2022
 ADD FILEGROUP ReadOnlyFG1;
 GO
@@ -112,7 +112,7 @@ ON
     a.object_id = b.object_id AND a.index_id = b.index_id;
 GO
 
---Task 6.1 Reorganize two indexes
+--Task 6.1: Reorganize two indexes
 USE AdventureWorksLT2022;
 ALTER INDEX PK_Address_AddressID
 ON
@@ -127,7 +127,7 @@ SalesLT.Address
 REORGANIZE;
 GO
 
---Task 6.2 Rebuild two indexes
+--Task 6.2: Rebuild two indexes
 USE AdventureWorksLT2022;
 ALTER INDEX AK_Address_rowguid
 ON
@@ -142,15 +142,15 @@ SalesLT.Address
 REBUILD;
 GO
 
---task 7 Backup database 
+--task 7: Backup database 
 BACKUP DATABASE AdventureWorksLT2022
 TO DISK = 'D:\SQL\AdventureWorksLT2022Backup.bak'
 
---task 7 Leave database prior to restore
+--task 7: Leave database prior to restore
 USE master;
 GO
 
---Task 8 create tables
+--Task 8: create tables
 USE AdventureWorksLT2022
 GO
 CREATE TABLE [dbo].[orders](
@@ -164,7 +164,7 @@ CREATE TABLE [dbo].[customers](
 [firstname] [nvarchar](50) NOT NULL
 ) ON [PRIMARY]
 GO
---Task 8 populate tables
+--Task 8: populate tables
 USE AdventureWorksLT2022
 GO
 INSERT INTO AdventureWorksLT2022.dbo.orders(orderid,amount,customer)
@@ -179,31 +179,119 @@ INSERT INTO AdventureWorksLT2022.dbo.customers(customerId,firstname)
 VALUES (2,'Allen Barbarosa');
 INSERT INTO AdventureWorksLT2022.dbo.customers(customerId,firstname)
 VALUES (4,'Debbie M"Lady');
---Task 8 Inner Join
+--Task 8: Inner Join
 USE AdventureWorksLT2022
 GO
 SELECT C.customerId, C.firstname, O.amount
 FROM dbo.customers as C 
 JOIN dbo.orders as O
 ON C.customerId = o.customer;
---Task 8 Full Outer Join
+--Task 8: Full Outer Join
 USE AdventureWorksLT2022
 GO
 SELECT C.customerId, C.firstname, O.amount
 FROM dbo.customers as C 
 FULL OUTER JOIN dbo.orders as O
 ON C.customerId = o.customer;
---Task 8 Left Outer Join
+--Task 8: Left Outer Join
 USE AdventureWorksLT2022
 GO
 SELECT C.customerId, C.firstname, O.amount
 FROM dbo.customers as C 
 LEFT JOIN dbo.orders as O
 ON C.customerId = o.customer;
---Task 8 Right Outer Join
+--Task 8: Right Outer Join
 USE AdventureWorksLT2022
 GO
 SELECT C.customerId, C.firstname, O.amount
 FROM dbo.customers as C 
 RIGHT JOIN dbo.orders as O
 ON C.customerId = o.customer;
+
+--Task 9.1: Logical Manipulation
+USE AdventureWorksLT2022
+GO
+SELECT firstName 
+FROM customers AS C 
+JOIN orders AS O
+ON C.customerId = O.customer
+WHERE O.amount <= 5  OR C.firstname = 'Allen Barbarosa';
+
+--Task 9.2: String Manipulation & Order By
+USE AdventureWorksLT2022
+GO
+	SELECT firstName 
+	FROM 
+	(SELECT ROW_NUMBER() OVER(ORDER BY C.customerId ASC) as rowNumber, value as firstName
+	FROM customers as C
+	CROSS APPLY STRING_SPLIT(C.firstname,' ')) as T
+WHERE rowNumber % 2 != 0;
+
+
+--Task 9.3: Like Manipulation
+USE AdventureWorksLT2022
+GO
+SELECT firstName 
+FROM customers as C
+WHERE C.firstname LIKE 'Allen%';
+
+--Task 9.4: Aggregate Function Manipulation
+USE AdventureWorksLT2022
+GO
+Select max(amount) from orders;
+
+--Task 9.5: Null & Not Null Manipulation
+USE AdventureWorksLT2022
+GO
+-- Null
+Select * from customers where firstname = '';
+-- Not Null
+Select * from customers where firstname != '';
+
+--Task 9.6: Group By Manipulation
+USE AdventureWorksLT2022
+GO
+UPDATE orders
+SET orderid = 640
+WHERE customer = 2;
+Select COUNT(customer), orderid from orders
+GROUP BY orderid;
+
+--Task 11
+USE AdventureWorksLT2022
+GO
+CREATE TABLE Employee
+(
+EmployeeID int NOT NULL,
+FirstName nvarchar(50) NOT NULL,
+LastName nvarchar(50) NOT NULL,
+HireDate date,
+);
+CREATE TABLE EmpLog (
+logID INT IDENTITY(1,1) NOT NULL
+, EmployeeID INT NOT NULL
+, FirstName NVARCHAR(50) NOT NULL
+, LastName NVARCHAR(50) NOT NULL
+, HireDate date NOT NULL
+, Operation NVARCHAR(50)
+, UpdatedOn DATETIME
+, UpdatedBy NVARCHAR(50)
+);
+GO
+--Create trigger
+CREATE TRIGGER trgEmployeeInsert
+ON Employee
+FOR INSERT
+AS INSERT INTO EmpLog(EmployeeID, FirstName, LastName,HireDate,Operation,UpdatedOn, UpdatedBy)
+SELECT EmployeeID, FirstName, LastName, HireDate, 'INSERT', GETDATE(), SUSER_NAME() FROM INSERTED;
+GO
+--Insert values
+INSERT INTO Employee
+VALUES(101, 'John','Rocks','05-12-2018'),
+(112, 'Peter','King','01-01-2015');
+GO
+--Display results of trigger
+SELECT *
+FROM EmpLog
+ORDER BY EmployeeID
+GO
